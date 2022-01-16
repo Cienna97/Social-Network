8dc8e26299f6fdd98b70c19cb90e752715c4350e
Cienna97 <cmortenson97@gmail.com>
2022-01-16 16:54:15 -0700

    thought-controllers function

---
+++ controllers/thought-controller.js
@@ -0,0 +1,47 @@
+
+//const Thought = require('../models/Thought');
+
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+
+
+const thoughtController = {
+  // get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+
+
+},
+getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'No thought with this id!' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
