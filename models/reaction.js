bdac6d1bcf6037c3df2fdb50986adb866df332da
Cienna97 <cmortenson97@gmail.com>
2022-01-16 19:37:36 -0700

    createdAt, username

--- models/reaction.js
+++ models/reaction.js
@@ -4,26 +4,42 @@ const dateFormat = require('../utils/dateFormat');
 
 //reactionID
 const reactionSchema = new Schema(
-{
+    {
+reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+},
+
+//reactionbody
+//string, required, 280 character max
     reactionBody: {
         type: String,
         required: true,
         maxlength: 280
     },
-    
-}
-)
-
-//reactionbody
-//string, required, 280 character max
-
 //username
 //string, required
-
+username: {
+    type: String,
+    required: true
+},
 //createdAt
 //date, Set default value to the current timestamp, Use a getter method to format the timestamp on query
+createdAt: {
+    type: Date,
 
+    default: Date.now,
+    get: timestamp => dateFormat(timestamp)
+}
 //Schema settings
 //This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+
+},
+{
+    toJSON: {
+        getters: true
+    }
+}
+)
 
 module.exports = reactionSchema
